3a93aee928bba8c8af61af4cca5f89cbe10adce3
salvadorMedina <samcas2702@gmail.com>
2024-08-21 22:01:42 -0500

    primer commit

---
+++ src/data/tasks.js
@@ -0,0 +1,22 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "task n 1",
+    description: "task n1",
+  },
+  {
+    id: 1,
+    title: "task n 2",
+    description: "task n2",
+  },
+  {
+    id: 2,
+    title: "task n 3",
+    description: "task n3",
+  },
+  {
+    id: 3,
+    title: "task n 4",
+    description: "task n4",
+  },
+];
